756079b516a0e24673c70d038135bd48962ab388
Payyavula-Prashanthi <129780371+Payyavula-Prashanthi@users.noreply.github.com>
2024-12-29 14:15:00 +0530

    Add files via upload

---
+++ Portfolio.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type",{
+    strings : ["MERN Developer","Programmer", "Web Developer","Full-Stack Developer"],
+    typedSpeed :140,
+    backSpeed : 140,
+    looped :true
+})
